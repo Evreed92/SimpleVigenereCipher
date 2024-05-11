6ecc1d4a166b4177f2ba53846ab0993e818f66bd
Evreed92 <103301462+Evreed92@users.noreply.github.com>
2024-05-11 16:09:39 -0500

    Add files via upload

---
+++ EncryptDecrypt/Level1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptDecrypt
+{
+    
+    internal class Level1
+    {
+        static char[] BasicAlphabet = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '};
+        
+        static string KeyWord = "EVAN AND OLIVIA REED";
+        static int key;
+
+
+        static public string Encrypt(string message)
+        {
+            string cipher = "";
+            int KeyWordIndex = 0;
+
+            foreach (char c in message)
+            {
+                //Console.WriteLine(c);
+
+                for (int i = 0; i < BasicAlphabet.Length; i++)
+                {
+                    if (KeyWord[KeyWordIndex] == BasicAlphabet[i])
+                    {
+                        key = i;
+                        //Console.WriteLine("Key = " + key);
+                        KeyWordIndex++;
+                        if (KeyWordIndex >= KeyWord.Length)
+                        {
+                            KeyWordIndex = 0;
+                        }
+                        break;
+                    }
+                }
+                for (int i = 0; i < BasicAlphabet.Length; i++)
+                {
+                    
+                    if (c == BasicAlphabet[i])
+                    {
+                        int keycode = (i + key) % BasicAlphabet.Length;
+                        //Console.Write(keycode);
+                        //Console.WriteLine(BasicAlphabet[keycode]);
+                        cipher += BasicAlphabet[keycode];
+                    }
+                }
+            }
+
+           return cipher;
+
+        }
+
+        static public string Decrypt(string cipher)
+        {
+            string message = "";
+            int KeyWordIndex = 0;
+
+            foreach (char c in cipher)
+            {
+                for (int i = 0; i < BasicAlphabet.Length; i++)
+                {
+                    if (KeyWord[KeyWordIndex] == BasicAlphabet[i])
+                    {
+                        key = i;
+                        //Console.WriteLine("Key = " + key);
+                        KeyWordIndex++;
+                        if (KeyWordIndex >= KeyWord.Length)
+                        {
+                            KeyWordIndex = 0;
+                        }
+                        break;
+                    }
+                }
+                for (int i = 0; i < BasicAlphabet.Length; i++)
+                {
+
+                    if (c == BasicAlphabet[i])
+                    {
+                       // Console.WriteLine(i + " ; " + key);
+                        int keycode = (i + (BasicAlphabet.Length - key)) % BasicAlphabet.Length;
+                       // Console.WriteLine(keycode);
+                       // Console.WriteLine(BasicAlphabet[keycode]);
+                        message += BasicAlphabet[keycode];
+                    }
+                }
+            }
+
+            return message;
+
+        }
+    }
+}
---
+++ EncryptDecrypt/Program.cs
@@ -0,0 +1,17 @@
+﻿using EncryptDecrypt;
+
+//Get Message
+Console.WriteLine("Write your message");
+string? myMessage = Console.ReadLine();
+Console.Clear();
+
+//Encrypt Message
+string encryptMessage = Level1.Encrypt(myMessage);
+Console.WriteLine("Encrypted: " + encryptMessage);
+Console.WriteLine();
+Console.WriteLine("Decrypting...press enter");
+Console.ReadLine();
+//Decrypt Message
+string decryptMessage = Level1.Decrypt(encryptMessage);
+Console.WriteLine("Decrypted: " + decryptMessage);
+
